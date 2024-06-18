3be7eb7fb8ef4628f9cd21ae9103466766208ca1
Rishikesh Chaudhari <122692581+rishiVC20@users.noreply.github.com>
2024-06-19 00:46:32 +0530

    Time: 12 ms (24.73%) | Memory: 18 MB (59.78%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int>a;
         int n = numbers.size();
-        for (int i=0; i<n; i++)
-        {
-            int k = target - numbers[i];
-            int x = upper_bound(numbers.begin(),numbers.end(),k)-numbers.begin();
-            x--;
-            if (x<n && numbers[x]+numbers[i]==target){
-                // a.push_back(i+1);
-                // a.push_back(x+1);
-                return {i+1,x+1};
-                break;
-            }
-        }
+        int i=0, j=n-1;
+        while (i<j){
+            if (numbers[i]+numbers[j] == target)
+                return {i+1,j+1};
+            else if (numbers[i]+numbers[j] < target) 
+                i++;
+            else
+                j--;       
+        } 
 
         return {};
     }
