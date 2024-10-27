0b2105658ef062ac75b7aa71d01db0923f0b9e59
Rishikesh Chaudhari <122692581+rishiVC20@users.noreply.github.com>
2024-10-27 20:01:39 +0530

    Time: 960 ms (30.00%) | Memory: 465.7 MB (30.00%) - LeetSync

---
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    #define ll long long
+    const int mod=1e9+7;
+public:
+    int lengthAfterTransformations(string s, int t) {
+        ll n=s.size();
+        vector<ll>a(26,0);
+        for(auto i:s){
+            a[i-'a']++;
+        }
+
+        for(ll i=0; i<t; i++){
+            vector<ll>v(26,0);
+            for(ll j=0; j<26; j++){
+                if(j==25){
+                    v[0] += (a[j]%mod);
+                    v[1] += (a[j]%mod);
+                    break;
+                }
+                v[j+1]=a[j];
+            }
+            a=v;
+        }
+
+        ll cn=0;
+        for(auto i:a){
+            cn += i;
+            cn=(cn%mod);
+        }
+
+        return cn;
+    }
+};
