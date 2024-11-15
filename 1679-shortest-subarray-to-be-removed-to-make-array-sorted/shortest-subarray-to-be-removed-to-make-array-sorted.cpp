cf041b1a50c34464cfce262004687e2f8d863fcd
Rishikesh Chaudhari <122692581+rishiVC20@users.noreply.github.com>
2024-11-16 02:20:27 +0530

    Time: 0 ms (100.00%) | Memory: 69.5 MB (67.21%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -20,28 +20,28 @@ public:
             else break;
         }    
         int ans=min(n-p-1,q);
-        int x=-1;
-        for(int i=0; i<=p; i++){
-            if(arr[i] <= arr[q]){
-                x++;
-            }
-            else break;
-        }
-        int y=n;
-        for(int i=n-1; i>=q; i--){
-            if(arr[i] >= arr[p]){
-                y--;
-            }
-            else break;
-        }
-        if(x != -1)
-            ans = min(ans,q-x-1);
-        if(p>0 && q<n-1){
-            if(arr[p-1]<=arr[q+1])
-                ans=min(ans,q-p+1);
-        }    
-        if(y != n)
-            ans = min(ans,y-p-1);  
+        // int x=-1;
+        // for(int i=0; i<=p; i++){
+        //     if(arr[i] <= arr[q]){
+        //         x++;
+        //     }
+        //     else break;
+        // }
+        // int y=n;
+        // for(int i=n-1; i>=q; i--){
+        //     if(arr[i] >= arr[p]){
+        //         y--;
+        //     }
+        //     else break;
+        // }
+        // if(x != -1)
+        //     ans = min(ans,q-x-1);
+        // if(p>0 && q<n-1){
+        //     if(arr[p-1]<=arr[q+1])
+        //         ans=min(ans,q-p+1);
+        // }    
+        // if(y != n)
+        //     ans = min(ans,y-p-1);  
         int i=0, j=q;
         while(i<=p && j<n){
             if(arr[i]<=arr[j]){
@@ -51,7 +51,7 @@ public:
             else j++;
         }      
 
-        cout<<p<<' '<<q<<' '<<x<<' '<<y;
+        // cout<<p<<' '<<q<<' '<<x<<' '<<y;
         return ans;
     }
 };
