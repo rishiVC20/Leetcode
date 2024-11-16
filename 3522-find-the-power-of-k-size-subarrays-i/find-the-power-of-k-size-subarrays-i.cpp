215ba68c7d8be2db83007777bb20ebd3632827cd
Rishikesh Chaudhari <122692581+rishiVC20@users.noreply.github.com>
2024-11-16 08:54:32 +0530

    Time: 0 ms (100.00%) | Memory: 33.2 MB (32.15%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n=nums.size();
-        int i=-1,j=-1;
-        vector<int>ans;
-        for(int i=0; i<=n-k; i++){
-            bool f=true;
-            int p;
-            for(int j=i; j<i+k-1; j++){
-                // cout<<nums[j]<<' ';
-                if(nums[j]>=nums[j+1] || (abs(nums[j]-nums[j+1])!=1)){
-                    f=false;
-                    // break;
-                }
-                p=nums[j];
+        if(k==1)
+            return nums;
+        vector<int>ans;    
+        int p=1;
+        int j=-1;
+        for(int i=1; i<n; i++){
+            if(nums[i] > nums[i-1] && abs(nums[i]-nums[i-1])==1){
+                p++;
+                if(p>k)
+                    p--;
             }
-            cout<<endl;
-            if(f)
-                ans.push_back(nums[i+k-1]);
             else{
-                ans.push_back(-1);
-            }    
-        }
+                p=1;
+            }
+            if(i>=k-1){
+                if(p==k)
+                    ans.push_back(nums[i]);
+                else
+                    ans.push_back(-1);    
+            }
+        }  
 
         return ans;
     }
