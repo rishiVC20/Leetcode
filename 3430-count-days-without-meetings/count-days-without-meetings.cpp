aeb72e65065717eb21c4bf8e60003b8e96e19d61
Rishikesh Chaudhari <122692581+rishiVC20@users.noreply.github.com>
2025-03-24 09:51:52 +0530

    Time: 107 ms (22.11%) | Memory: 150.6 MB (14.12%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -1,35 +1,50 @@
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
-        vector<int>a;
-        vector<int>b;
-        int n = meetings.size();
-        sort(meetings.begin(), meetings.end(), [](const vector<int>& a, const vector<int>& b) {
-        return a[0] < b[0];
-    });
-        int cn = 0;
-        for (auto i:meetings)
-        {
-            a.push_back(i[0]);
-            b.push_back(i[1]);
-            // cn += (i[1]-i[0])+1;
+        vector<pair<int,int>>v;
+        for(auto i:meetings){
+            v.push_back({i[0],i[1]});
         }
-
-        cn += (a[0]-1);
-        for (int i=1; i<n; i++)
-        {
-            if (a[i] <= b[i-1])
-            {
-                if (b[i] <= b[i-1])
-                    b[i] = b[i-1];
+        sort(v.begin(),v.end(),[&](const pair<int,int>&a, const pair<int,int>&b){
+            if(a.first==b.first)
+                return a.second<b.second;
+            return a.first<b.first;
+        });
+        // for(auto i:v){
+        //     cout<<i.first<<' '<<i.second<<endl;
+        // }
+        vector<pair<int,int>>p;
+        int pre=v[0].first,las=v[0].second;
+        if(v.size()==1){
+            p.push_back({pre,las});
+        }
+        for(int i=1; i<(int)v.size(); i++){
+            if(v[i].first<=las){
+                las=max(las,v[i].second);
+                
             }
             else{
-                cn += a[i] - b[i-1] - 1;
+                p.push_back({pre,las});
+                pre=v[i].first,las=v[i].second;
+            }
+            if(i==(int)v.size()-1){
+                p.push_back({pre,las});
             }
         }
 
-        cn += days - b[n-1];
-
-        return cn;
+        // for(auto i:p){
+        //     cout<<i.first<<' '<<i.second<<endl;
+        // }
+        int cp=p[0].first-1;
+        for(int i=1; i<(int)p.size(); i++){
+            int t=p[i].first-p[i-1].second-1;
+            cp+=t;
+            // if(i==(int)p.size()-1){
+            //     cp+=days-p[i].second;
+            // }
+        }
+        cp+=days-p[(int)p.size()-1].second;
+        return cp;
     }
+
 };
