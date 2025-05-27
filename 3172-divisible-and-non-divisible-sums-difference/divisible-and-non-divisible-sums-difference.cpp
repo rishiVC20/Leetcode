edd117aca48301b38e237c92cc06ccde783e515e
Rishikesh Chaudhari <122692581+rishiVC20@users.noreply.github.com>
2025-05-27 10:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (94.16%) - LeetSync

---
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int num1=n*(n+1)/2;
+        int num2=0;
+        for(int i=m; i<=n; i+=m){
+            num2+=i;
+        }
+        num1-=num2;
+
+        return num1-num2;
+    }
+};
