4aaa54efd1bbf679a8cc49ca6b5c5abbcb1752cc
Rishikesh Chaudhari <122692581+rishiVC20@users.noreply.github.com>
2025-06-08 00:16:53 +0530

    Time: 215 ms (9.89%) | Memory: 110.5 MB (21.34%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    bool solve(int ind, int cs, int tar, vector<int> &nums, vector<vector<int>> &dp){
-        if(cs==tar){
-            return true;
-        }
-        if(ind==nums.size() || cs>tar){
-            return false;
-        }
-        if(dp[ind][cs] != -1){
-            return dp[ind][cs];
-        }
-
-        bool f1=solve(ind+1,cs+nums[ind],tar,nums,dp);
-        bool f2=solve(ind+1,cs,tar,nums,dp);
-
-        return dp[ind][cs] = f1 || f2;
-    }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=accumulate(nums.begin(),nums.end(),0);
         if(sum&1){
             return false;
         }
-        int tar=sum/2;
+        int md=sum/2;
+        vector<vector<int>>dp(n,vector<int>(md+1,-1));
+        auto solve=[&](auto &self, int ind, int cal)->bool{
+            if(ind==n || cal>md){
+                return false;
+            }
+            if(cal==md){
+                return true;
+            }
 
-        vector<vector<int>>dp(n+1,vector<int>(tar+1,-1));
-        return solve(0,0,tar,nums,dp);
+            if(dp[ind][cal]!=-1){
+                return dp[ind][cal];
+            }
+
+            // cal+=nums[ind];
+            bool t1=self(self,ind+1,cal+nums[ind]);
+            // cal-=nums[ind];
+            bool t2=self(self,ind+1,cal);
+
+            return dp[ind][cal]=t1|t2;
+
+        };
+
+        return solve(solve,0,0);
     }
 };
