65c9ff2b35a120920231367503f859f67b10aa1f
Rishikesh Chaudhari <122692581+rishiVC20@users.noreply.github.com>
2025-06-10 14:12:34 +0530

    Time: 3 ms (34.81%) | Memory: 9.6 MB (56.13%) - LeetSync

---
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxDifference(string s) {
+        int maxi=0,mini=INT_MAX;
+        map<int,int>mp;
+        for(auto i:s){
+            mp[i]++;
+        }
+        for(auto i:mp){
+            if(i.second&1){
+                maxi=max(maxi,i.second);
+            }
+            else{
+                mini=min(mini,i.second);
+            }
+        }
+
+        if(mini==INT_MAX){
+            mini=0;
+        }
+        return maxi-mini;
+    }
+};
